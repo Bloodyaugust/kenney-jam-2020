a02e8615a7891d17156503319985a88a85ab79ab
Bloodyaugust <greysonrichey@gmail.com>
2020-08-22 15:47:40 -0600

    Add background and background scrolling

---
+++ Assets/Scripts/Behaviors/Background.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+    public float ScrollSpeed;
+
+    private float yOffset;
+    private SpriteRenderer spriteRenderer;
+
+    void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update() {
+        yOffset += Time.deltaTime * ScrollSpeed;
+
+        spriteRenderer.material.mainTextureOffset = new Vector2(0, yOffset);
+    }
+}
